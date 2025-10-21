a5337ac6eeb9666afb71b239718b2f80eb29c7ea
najeeb678 <najibqazi777@gmail.com>
2025-10-21 18:10:07 +0500

    updated task image upload logic

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -8,3 +8,8 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
 //   throw new Error("❌ Missing Supabase environment variables");
 // }
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+
+// NEXT_PUBLIC_SUPABASE_URL=https://vannabdnnslosolymnyr.supabase.co
+// NEXT_PUBLIC_SUPABASE_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhbm5hYmRubnNsb3NvbHltbnlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5MTkyMjEsImV4cCI6MjA3NjQ5NTIyMX0.dftWaIo1G1L2VuUjsG3b_HxYJwvjp04yEu-AXM5lDek
+
